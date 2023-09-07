e1c407abc5c64579f93272a069eab1a9fbf99f41
FriedCerebrum <iw.kuz8@gmail.com>
2023-09-08 01:22:45 +0300

    Fixed bug with folder disposition

--- BackEnd/WebServer.py
+++ BackEnd/WebServer.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='SitePages/GlobalPage', static_folder='SitePages/GlobalPage')
 
 @app.route("/")
 def index():
     return render_template("GlobalPage.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8087)
