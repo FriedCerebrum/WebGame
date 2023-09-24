06b5392f5bf6073c9b3d4f0870e8e36df09e5f7d
FriedCerebrum <iw.kuz8@gmail.com>
2023-09-24 23:54:10 +0300

    Trying to correctly start the web-server. Not working at this time

--- BackEnd/WebServer.py
+++ BackEnd/WebServer.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='SitePages/GlobalPage', static_folder='SitePages/GlobalPage')
+app = Flask(__name__, template_folder='../FrontEnd/SitePages', static_folder='../FrontEnd/SitePages')
 
 @app.route("/")
 def index():
+    # Здесь можно вернуть главную страницу или какой-то общий шаблон
+    return render_template("MainPage/MainPage.html")
+
+@app.route("/<path:page_name>/")
+def render_page(page_name):
+    template_path = f"{page_name}/Page.html"  # Формируем путь к шаблону страницы
+    return render_template(template_path)
+
+@app.route("/login")
+def login():
+    return render_template("LogInPage.html")
+
+@app.route("/main")
+def main():
     return render_template("MainPage.html")
+
+@app.route("/signup")
+def signup():
+    return render_template("SignUpPage.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8087)
