db1029ce824ccc357e1b3d1bd0e91869264c3135
Memory420 <ihatesspies@gmail.com>
2023-10-15 14:52:20 +0300

    Update 3

--- Clash of heroes/Assets/Scripts/PlayerMovement.cs
+++ Clash of heroes/Assets/Scripts/PlayerMovement.cs
@@ -7,20 +7,35 @@ public class PlayerMovement : MonoBehaviour
 
     public float runSpeed = 40f;
 
+    [SerializeField] private float startCrouchSpeed;
+
     public float horizontalMove = 0f;
 
     bool jump = false;
     bool crouch = false;
+    private void Start()
+    {
+        startCrouchSpeed = controller.m_CrouchSpeed;
+    }
 
     private void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
 
+        if (controller.IsGrounded())
+        {
+            controller.m_CrouchSpeed = 1;
+        }
+        else
+        {
+            controller.m_CrouchSpeed = startCrouchSpeed;
+        }
+
         if (Input.GetButtonDown("Jump"))
         {
             jump = true;
         }
-        if (Input.GetButtonDown("Crouch"))
+        if (Input.GetButtonDown("Crouch") && controller.IsGrounded())
         {
             crouch = true;
         } else if (Input.GetButtonUp("Crouch"))
