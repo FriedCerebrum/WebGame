a7a67e8528a58297cfa2c613750bb59882b1b1e6
Memory420 <ihatesspies@gmail.com>
2023-10-29 21:19:01 +0300

    Added attack delay

--- Clash of heroes/Assets/Scripts/PlayerCombat.cs
+++ Clash of heroes/Assets/Scripts/PlayerCombat.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCombat : MonoBehaviour
@@ -7,25 +6,37 @@ public class PlayerCombat : MonoBehaviour
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public LayerMask enemyLayers;
+    public float attackSpeed = 0.1f;
+    private bool ableToAttack = true;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0) && ableToAttack)
         {
-            Attack();
+            StartCoroutine(TimeToAttack());
         }
     }
+
+    IEnumerator TimeToAttack()
+    {
+        ableToAttack = false;
+        yield return new WaitForSeconds(attackSpeed);
+        Attack();
+        ableToAttack = true;
+    }
+
     void Attack()
     {
         Debug.Log("Attack!");
-        Collider2D[] hitEnemies =  Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
 
-        foreach(Collider2D enemy in hitEnemies)
+        foreach (Collider2D enemy in hitEnemies)
         {
             Debug.Log("We hit " + enemy.name);
             enemy.GetComponent<Entity>().TakeDamage(Random.Range(25, 35));
         }
     }
+
     private void OnDrawGizmosSelected()
     {
         if (attackPoint == null)
