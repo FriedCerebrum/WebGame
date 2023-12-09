2fbace29b0276dcedc624d1683e22c24d9de77a6
FriedCerebrum <iw.kuz8@gmail.com>
2023-12-09 18:19:25 +0300

    0

--- Clash of heroes/Assets/PlayerSpawnManager.cs
+++ Clash of heroes/Assets/PlayerSpawnManager.cs
@@ -14,19 +14,24 @@ public class PlayerSpawnManager : MonoBehaviourPun
             return;
         }
 
+        Debug.Log("PlayerSpawnManager Start() called.");
+
         if (PhotonNetwork.IsMasterClient)
         {
+            Debug.Log("Master client detected. Spawning player...");
             SpawnPlayer(); // Мастер клиент спавнит себя и отправляет информацию о точке спавна другим
         }
         else
         {
+            Debug.Log("Non-master client detected. Requesting spawn point...");
             RequestSpawnPoint(); // Немастер клиент запрашивает точку спавна
         }
     }
 
-
     void SpawnPlayer(int spawnIndex = -1)
     {
+        Debug.Log("SpawnPlayer() called with spawnIndex: " + spawnIndex);
+
         if (spawnPoints == null || spawnPoints.Length == 0)
         {
             Debug.LogError("Spawn points array is not initialized or empty!");
@@ -39,6 +44,7 @@ public class PlayerSpawnManager : MonoBehaviourPun
         {
             // Рандомный выбор точки спавна для мастер клиента
             spawnIndex = Random.Range(0, spawnPoints.Length);
+            Debug.Log("Random spawn index selected: " + spawnIndex);
         }
 
         spawnPoint = spawnPoints[spawnIndex];
@@ -49,6 +55,8 @@ public class PlayerSpawnManager : MonoBehaviourPun
             Debug.LogError("Spawn point at index " + spawnIndex + " is null!");
             return;
         }
+
+        Debug.Log("Spawning player at spawn point index: " + spawnIndex);
 
         GameObject playerPrefab = Resources.Load<GameObject>("Player 2");
         if (playerPrefab == null)
@@ -63,25 +71,26 @@ public class PlayerSpawnManager : MonoBehaviourPun
             return;
         }
 
-
         PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.transform.position, Quaternion.identity);
 
         if (PhotonNetwork.IsMasterClient)
         {
             // Отправляем информацию о точке спавна остальным игрокам
             photonView.RPC("ReceiveSpawnPoint", RpcTarget.Others, spawnIndex);
+            Debug.Log("Sent spawn point information to other players.");
         }
     }
-
 
     [PunRPC]
     void ReceiveSpawnPoint(int spawnIndex)
     {
+        Debug.Log("Received spawn point request. Spawning player at index: " + spawnIndex);
         SpawnPlayer(spawnIndex);
     }
 
     void RequestSpawnPoint()
     {
+        Debug.Log("Requesting spawn point from master client.");
         photonView.RPC("RequestSpawnPointFromMaster", RpcTarget.MasterClient);
     }
 
@@ -93,6 +102,7 @@ public class PlayerSpawnManager : MonoBehaviourPun
             // Отправляем информацию о точке спавна запрашивающему
             int spawnIndex = Random.Range(0, spawnPoints.Length);
             photonView.RPC("ReceiveSpawnPoint", photonView.Owner, spawnIndex);
+            Debug.Log("Sent spawn point information to requesting client.");
         }
     }
 }
