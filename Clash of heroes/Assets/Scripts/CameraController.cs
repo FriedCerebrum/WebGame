f11148b349be25e146b63a61aff13f9f9a795e8b
Memory420 <ihatesspies@gmail.com>
2023-12-09 17:42:24 +0300

    Dev sandbox

--- Clash of heroes/Assets/Scripts/CameraController.cs
+++ Clash of heroes/Assets/Scripts/CameraController.cs
@@ -25,24 +25,27 @@ public class CameraController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        // Вычисляем центр между двумя бойцами
-        Vector3 centerPoint = (target1.position + target2.position) / 2f;
+        if (target1 != null && target2 != null)
+        {
+            // Вычисляем центр между двумя бойцами
+            Vector3 centerPoint = (target1.position + target2.position) / 2f;
 
-        // Вычисляем новую Y-координату центральной точки
-        float newY = Mathf.Clamp(centerPoint.y, centralPoint.position.y - maxYOffset, centralPoint.position.y + maxYOffset);
+            // Вычисляем новую Y-координату центральной точки
+            float newY = Mathf.Clamp(centerPoint.y, centralPoint.position.y - maxYOffset, centralPoint.position.y + maxYOffset);
 
-        // Учитываем максимальное вертикальное смещение
-        centerPoint.y = newY;
+            // Учитываем максимальное вертикальное смещение
+            centerPoint.y = newY;
 
-        // Вычисляем новый размер зума
-        float distance = Vector3.Distance(target1.position, target2.position);
-        float targetSize = Mathf.Clamp(distance * 0.5f, minZoom, maxZoom);
+            // Вычисляем новый размер зума
+            float distance = Vector3.Distance(target1.position, target2.position);
+            float targetSize = Mathf.Clamp(distance * 0.5f, minZoom, maxZoom);
 
-        // Плавно перемещаем камеру к новой позиции
-        Vector3 newPosition = Vector3.Lerp(transform.position, centerPoint, smoothSpeed * Time.deltaTime);
-        transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
+            // Плавно перемещаем камеру к новой позиции
+            Vector3 newPosition = Vector3.Lerp(transform.position, centerPoint, smoothSpeed * Time.deltaTime);
+            transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
 
-        // Плавно меняем размер камеры (зум)
-        mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, targetSize, zoomSpeed * Time.deltaTime);
+            // Плавно меняем размер камеры (зум)
+            mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, targetSize, zoomSpeed * Time.deltaTime);
+        }
     }
 }
