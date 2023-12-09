25867e962defe1292b9ab3802e53c6058c6a1b27
Memory420 <ihatesspies@gmail.com>
2023-12-09 21:57:58 +0300

    Update #59704

--- Clash of heroes/Assets/Scripts/DevSpawn.cs
+++ Clash of heroes/Assets/Scripts/DevSpawn.cs
@@ -14,10 +14,12 @@ public class DevSpawn : MonoBehaviour
     {
         Vector2 randomPosition = new Vector2(Random.Range(minX, minY), Random.Range(maxX, maxY));
         PhotonNetwork.Instantiate(player.name, randomPosition, Quaternion.identity);
+        Debug.Log("Вы появились, Мисье.");
     }
     public void SpawnOtherPlayer(Photon.Realtime.Player player)
     {
         Vector2 randomPosition = new Vector2(Random.Range(minX, maxX), Random.Range(minY, maxY));
         PhotonNetwork.Instantiate("PlayerMain", randomPosition, Quaternion.identity);
+        Debug.Log("Заспавнен игрок");
     }
 }
